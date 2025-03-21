b0a9133b2bcf177f695fdc2082e8844f065d3275
Arvin Santander <vincarxstreet@gmail.com>
2025-03-21 20:41:41 +0800

    Add files via upload

---
+++ CS1A_MTExam_Santander/CS1A_SANTANDER_Prob1.js
@@ -0,0 +1,14 @@
+// userProfile
+
+// Ask the user to enter their details
+let fullName = prompt("Enter your full name:");
+let age = prompt("Enter your age:");
+let favoriteNumber = prompt("Enter your favorite number:");
+let favoriteColor = prompt("Enter your favorite color:");
+
+// Display the collected information in the console
+console.log("User Information:");
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
